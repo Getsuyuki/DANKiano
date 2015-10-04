66b51cc3669f0b0e23cdd005efbef4ae758ca8c8
Calseon <chungzheng@gmail.com>
2015-10-04 15:13:06 -0400

    Updated the styles and view xmls. Current issue is that all the buttons are stacked ontop of each other. See file activity_piano_main.xml

--- MyApplication/app/src/main/java/dankmemers/dankiano/PianoMain.java
+++ MyApplication/app/src/main/java/dankmemers/dankiano/PianoMain.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.*;
 
 
 public class PianoMain extends ActionBarActivity {
@@ -35,5 +36,10 @@ public class PianoMain extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_whitekey(View v)
+    {
+
     }
 }
