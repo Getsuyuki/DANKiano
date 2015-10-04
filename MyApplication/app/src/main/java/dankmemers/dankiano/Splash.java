3fc82054d00b2a0310353e5b141013c6ee701f15
David Li <li.zq.david@gmail.com>
2015-10-04 19:10:05 -0400

    swoi
    swoi

--- MyApplication/app/src/main/java/dankmemers/dankiano/Splash.java
+++ MyApplication/app/src/main/java/dankmemers/dankiano/Splash.java
@@ -1,9 +1,12 @@
 package dankmemers.dankiano;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
+import android.content.res.Resources;
 
 /**
  * Created by David on 04/10/2015.
@@ -16,10 +19,25 @@ public class Splash extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
+        final MediaPlayer start = new MediaPlayer();
+
+        try{
+            AssetFileDescriptor afd;
+            afd = getAssets().openFd("CENA.wav");
+            start.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            start.prepare();
+            start.start();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 Intent i = new Intent(Splash.this, PianoMain.class);
+                start.stop();
+
                 startActivity(i);
 
                 finish();
